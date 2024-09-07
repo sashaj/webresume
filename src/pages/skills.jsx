898e032943a0ac59cccb8ca6002cfb0e54a30902
git_adf <alexanderivanovset@gmail.com>
2024-09-07 12:01:26 +0900

    fixes

--- src/pages/skills.jsx
+++ src/pages/skills.jsx
@@ -2,7 +2,7 @@ import Text from "../components/text";
 
 export default function skills() {
     const skillText = [
-        { key: "1sk", text: "HTML5, CSS3, Scss, Javascript(ES 6+)" },
+        { key: "1sk", text: "JavaScript, TypeScript, HTML5, CSS3, SCSS, Tailwind" },
         { key: "2sk", text: "Vue stack: Vue 2/3, Nuxt, Options/Composition API, Pinia, Router" },
         { key: "3sk", text: "React stack: React 18, Pinia, Router" },
         { key: "4sk", text: "Languages: Russian Native, English C1" },
