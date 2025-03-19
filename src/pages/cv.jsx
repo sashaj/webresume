74cc3caa6b36112497bfd093f789aebd5bf68b61
git_adf <alexanderivanovset@gmail.com>
2025-03-19 12:32:00 +0900

    dev

--- src/pages/cv.jsx
+++ src/pages/cv.jsx
@@ -3,7 +3,7 @@ import Text from "../components/text";
 export default function works() {
     const cvText = [
         { key: "1cv", link: "https://drive.google.com/file/d/1e6FwWlAi93XhFwxNd98d2nv0zLRThMhi/view?usp=sharing", text: "Russian.pdf" },
-        { key: "2cv", link: "https://drive.google.com/file/d/182x97RaS9COFNxQu15a7GVAm0PTNsfYB/view?usp=sharing", text: "English.pdf" },
+        { key: "2cv", link: "https://drive.google.com/file/d/1iBVkJ5cH_aQP-neP0FKmdximDzohcn1a/view?usp=sharing", text: "English.pdf" },
     ];
 
     return <Text items={cvText}></Text>;
