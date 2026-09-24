f597560269460a8e6e75058483ee2a7742408f62
Alexander Ivanov <alexanderivanovset@gmail.com>
2026-09-24 08:30:42 +0700

    change cvs

--- src/pages/cv.jsx
+++ src/pages/cv.jsx
@@ -2,8 +2,8 @@ import Text from "../components/text";
 
 export default function works() {
     const cvText = [
-        { key: "1cv", link: "https://drive.google.com/file/d/1efPpqmave8WzRxaKiXEYvPCzTenfkuI6/view?usp=sharing", text: "Russian.pdf" },
-        { key: "2cv", link: "https://drive.google.com/file/d/11bwjBUfwBeiFdYHNCfGOxqCl1uE8KGV4/view?usp=sharing", text: "English.pdf" },
+        { key: "1cv", link: "https://drive.google.com/file/d/1t1SwNqdu3XsRBna7oWJWl5CMg3xBo6IZ/view?usp=sharing", text: "Russian.pdf" },
+        { key: "2cv", link: "https://drive.google.com/file/d/1B4BcM3BcxXN2FCvZmjZ_9uZ5L6DNle7N/view?usp=sharing", text: "English.pdf" },
     ];
 
     return <Text items={cvText}></Text>;
